80701e4ae8dcb3eab4cf28120b504ef93e584d88
u21447234 <u21447234@tuks.co.za>
2022-08-21 00:46:48 +0200

    home and about done

--- u21447234_HW04/u21447234_HW04/Controllers/HomeController.cs
+++ u21447234_HW04/u21447234_HW04/Controllers/HomeController.cs
@@ -15,29 +15,21 @@ namespace u21447234_HW04.Controllers
 
         public ActionResult Donate()
         {
-            ViewBag.Message = "Donate";
-
             return View();
         }
 
         public ActionResult Volunteer()
         {
-            ViewBag.Message = "Volunteer";
-
             return View();
         }
 
         public ActionResult Petition()
         {
-            ViewBag.Message = "Petition";
-
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
     }
---
+++ u21447234_HW04/u21447234_HW04/Models/Petition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u21447234_HW04.Models
+{
+    public class Petition:User
+    {
+
+
+    }
+}
